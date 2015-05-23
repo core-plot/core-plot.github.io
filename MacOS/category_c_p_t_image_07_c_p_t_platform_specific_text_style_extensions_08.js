0f89e47aa62f3d9949204194e3651cfd7d91c145
Eric Skroch <eskroch@mac.com>
2015-05-23 15:22:47 -0400

    Built new HTML docs.

--- MacOS/category_c_p_t_image_07_c_p_t_platform_specific_text_style_extensions_08.js
+++
@@ -1,5 +0,0 @@
-var category_c_p_t_image_07_c_p_t_platform_specific_text_style_extensions_08 =
-[
-    [ "initForPNGFile:", "category_c_p_t_image_07_c_p_t_platform_specific_text_style_extensions_08.html#ae8b93339887493c0104f17b9fe7510b1", null ],
-    [ "initWithNativeImage:", "category_c_p_t_image_07_c_p_t_platform_specific_text_style_extensions_08.html#a28781648e72eb92c319773a268943ade", null ]
-];
